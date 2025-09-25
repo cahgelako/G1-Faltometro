14643a2c7aa3c16d1eb23c2f6ceaac9142836b13
anamennd <164012224+cahgelako@users.noreply.github.com>
2025-09-25 07:09:45 -0300

    Update footer.php

---
+++ app/views/layout/footer.php
@@ -0,0 +1,4 @@
+<footer class="text-white text-center py-3 mt-5" style="background-color: #99ced4;">Produzido por Grupo 1</footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+        <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
