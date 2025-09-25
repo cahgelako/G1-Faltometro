118ddb089197d2b57f596c8ceba279afb3938f54
beatrizdacruzpurissimo10 <biacruzpurissimo@gmail.com>
2025-09-25 15:28:48 -0300

    Criação Model e Controller da tabela Turmas

--- routes/routes.php
+++ routes/routes.php
@@ -2,13 +2,17 @@
 $routes = [
 
     // Site (revisar)
-    'sobre'       => ['controller' => 'SiteController', 'method' => 'sobre'],
-    'contato'     => ['controller' => 'SiteController', 'method' => 'contato'],
+    'sobre'    => ['controller' => 'SiteController', 'method' => 'sobre'],
+    'contato'  => ['controller' => 'SiteController', 'method' => 'contato'],
     'inicio'   => ['controller' => 'SiteController', 'method' => 'inicio'],
-    'logout'      => ['controller' => 'UserController', 'method' => 'logout'],
+    'logout'   => ['controller' => 'UserController', 'method' => 'logout'],
 
     // Escolas
     'listEscola' => ['controller' => 'EscolaController', 'method' => 'listar'],
     'registerEs' => ['controller' => 'EscolaController', 'method' => 'registrar'],
-    'editEscola' => ['controller' => 'EscolaController', 'method' => 'editar']
+    'editEscola' => ['controller' => 'EscolaController', 'method' => 'editar'],
+
+    // Turmas
+    'listTurma' => ['controller' => 'TurmaController', 'method' => 'listar'],
+    'registerTu'=> ['controller' => 'TurmaController', 'method' => 'registrar']
 ];
