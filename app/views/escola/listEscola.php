9e3cfdd51b287bf1447afbf488587b6564760927
cahgelako <164012224+cahgelako@users.noreply.github.com>
2025-10-09 10:45:11 -0300

    adição do link do JavaScript(cabeçalho))

--- app/views/escola/listEscola.php
+++ app/views/escola/listEscola.php
@@ -6,7 +6,7 @@
             <div class="card shadow-lg">
                 <div class="card-body">
                     <div class="col-md-12 mb-3">
-                        <a href="./registerEs" class="btn btn-danger">Cadastrar</a>
+                        <a href="./registerEs" class="btn btn-primary">Cadastrar</a>
                     </div>
                     <div class="table-responsive">
                         <table id="idtabela" class="table table-bordered" cellpadding = '5'>
@@ -45,3 +45,4 @@
     </div>
 
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
